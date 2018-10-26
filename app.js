25801ce71c027889c18fc35e324f3b08f6d91a07
gourabk121 <44121563+gourabk121@users.noreply.github.com>
2018-10-26 21:52:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+
+function btnplus(){
+  document.my.text.value+="+";
+}
+function btnmin(){
+  document.my.text.value+="-";
+}
+function btnmul(){
+  document.my.text.value+="*";
+}
+function btndiv(){
+  document.my.text.value+="/";
+}
+function btndot(){
+  document.my.text.value+=".";
+}
+function btnclr(){
+
+  document.form.text.value="";
+}
